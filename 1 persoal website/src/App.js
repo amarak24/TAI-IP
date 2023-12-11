cd43f93160ec240cef3de457652dea946971a70b
Amar Klaukhe <86222394+amarak24@users.noreply.github.com>
2023-12-11 20:59:37 +0530

    Add files via upload

---
+++ 1 persoal website/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  return (
+
+   <>
+  
+   <Header/>
+   <Home/>
+  
+   
+   </>
+  );
+}
+
+export default App;
